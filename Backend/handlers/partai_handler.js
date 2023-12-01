eb439c5ebdd56eb2fe4d2d8f47076216372f5c23
feliciakiani <feliciakiani103@gmail.com>
2023-12-01 15:58:13 +0700

    partai endpoint

--- Backend/handlers/partai_handler.js
+++ Backend/handlers/partai_handler.js
@@ -3,12 +3,63 @@ const pool = require('../db_server');
 const getAllPartai = async (request, h) => {
     try {
         const connection = await pool.getConnection();
-        
-        const [rows] = await connection.execute('SELECT * FROM partai');
+
+        const query = `
+            SELECT 
+                partai.ID,
+                partai.Nama,
+                partai.Akronim,
+                partai.Logo,
+                partai.KetuaUmum,
+                partai.SekretarisJenderal,
+                partai.KetuaFraksiDPR,
+                partai.TglDibentuk,
+                partai.KantorPusat,
+                partai.Ideologi,
+                partai.JmlKursiDPR,
+                partai.JmlKursiDPRD1,
+                partai.JmlKursiDPRD2,
+                GROUP_CONCAT(DISTINCT visi_partai.Visi ORDER BY visi_partai.ID SEPARATOR '|') AS Visi,
+                GROUP_CONCAT(DISTINCT misi_partai.Misi ORDER BY misi_partai.ID SEPARATOR '|') AS Misi,
+                COUNT(favorit_partai.IDPartai) AS Favorit
+            FROM
+                partai
+            LEFT JOIN
+                visi_partai ON partai.ID = visi_partai.IDPartai
+            LEFT JOIN
+                misi_partai ON partai.ID = misi_partai.IDPartai
+            LEFT JOIN
+                favorit_partai ON favorit_partai.IDPartai = partai.ID
+            GROUP BY
+                partai.ID;
+        `;
+
+        const [rows] = await connection.execute(query);
 
         connection.release();
 
-        return h.response(rows).code(200);
+        const partaiData = rows.map(row => ({
+            ID: row.ID,
+            Nama: row.Nama,
+            Akronim: row.Akronim,
+            Logo: row.Logo,
+            KetuaUmum: row.KetuaUmum,
+            SekretarisJenderal: row.SekretarisJenderal,
+            KetuaFraksiDPR: row.KetuaFraksiDPR,
+            TglDibentuk: row.TglDibentuk,
+            KantorPusat: row.KantorPusat,
+            Ideologi: row.Ideologi,
+            Kursi: {
+                DPR: row.JmlKursiDPR,
+                DPRD1: row.JmlKursiDPRD1,
+                DPRD2: row.JmlKursiDPRD2,
+            },
+            Visi: row.Visi ? row.Visi.split('|').filter(Boolean) : {},
+            Misi: row.Misi ? row.Misi.split('|').filter(Boolean) : {},
+            Favorit: row.Favorit,
+        }));
+
+        return h.response(partaiData).code(200);
 
     } catch (error) {
         console.error('Error retrieving data:', error);
@@ -26,13 +77,65 @@ const getPartaiById = async (request, h) => {
             return h.response('Invalid ID parameter').code(400);
         }
 
-        const [rows] = await connection.execute('SELECT * FROM partai WHERE ID=?', [partaiId]);
+        const query = `
+            SELECT 
+                partai.ID,
+                partai.Nama,
+                partai.Akronim,
+                partai.Logo,
+                partai.KetuaUmum,
+                partai.SekretarisJenderal,
+                partai.KetuaFraksiDPR,
+                partai.TglDibentuk,
+                partai.KantorPusat,
+                partai.Ideologi,
+                partai.JmlKursiDPR,
+                partai.JmlKursiDPRD1,
+                partai.JmlKursiDPRD2,
+                GROUP_CONCAT(DISTINCT visi_partai.Visi ORDER BY visi_partai.ID SEPARATOR '|') AS Visi,
+                GROUP_CONCAT(DISTINCT misi_partai.Misi ORDER BY misi_partai.ID SEPARATOR '|') AS Misi,
+                COUNT(favorit_partai.IDPartai) AS Favorit
+            FROM
+                partai
+            LEFT JOIN
+                visi_partai ON partai.ID = visi_partai.IDPartai
+            LEFT JOIN
+                misi_partai ON partai.ID = misi_partai.IDPartai
+            LEFT JOIN
+                favorit_partai ON favorit_partai.IDPartai = partai.ID
+            WHERE
+                partai.ID=?
+            GROUP BY
+                partai.ID
+        `;
+
+        const [rows] = await connection.execute(query, [partaiId]);
 
         connection.release();
 
         if (rows.length > 0) {
-            const anggotaPartai = rows[0];
-            return h.response(anggotaPartai).code(200);
+            const partaiData = rows.map(row => ({
+                ID: row.ID,
+                Nama: row.Nama,
+                Akronim: row.Akronim,
+                Logo: row.Logo,
+                KetuaUmum: row.KetuaUmum,
+                SekretarisJenderal: row.SekretarisJenderal,
+                KetuaFraksiDPR: row.KetuaFraksiDPR,
+                TglDibentuk: row.TglDibentuk,
+                KantorPusat: row.KantorPusat,
+                Ideologi: row.Ideologi,
+                Kursi: {
+                    DPR: row.JmlKursiDPR,
+                    DPRD1: row.JmlKursiDPRD1,
+                    DPRD2: row.JmlKursiDPRD2,
+                },
+                Visi: row.Visi ? row.Visi.split('|').filter(Boolean) : {},
+                Misi: row.Misi ? row.Misi.split('|').filter(Boolean) : {},
+                Favorit: row.Favorit
+            }));
+
+            return h.response(partaiData).code(200);
         } else {
             return h.response('Partai not found').code(404);
         }
