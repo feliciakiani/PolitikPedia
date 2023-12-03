a08f55362b58e212ad1dfb16b1cf5d2abee1bc64
dicozaalex <juan.dicoza@gmail.com>
2023-12-03 15:39:15 +0700

    added user register

---
+++ Backend/handlers/user_handler.js
@@ -0,0 +1,43 @@
+const pool = require('../db_server');
+
+const checkEmailExists = async(email) => {
+    const connection = await pool.getConnection();
+    const query = 'SELECT ID FROM `user` WHERE email = ?';
+    const [rows] = await connection.execute(query, [email]);
+    connection.release();
+    return rows.length>0;
+};
+
+const userRegister = async (request, h) => {
+ try {
+    const { firstName, lastName, email, password } = request.payload;
+    const connection = await pool.getConnection();
+
+    // check parameter
+    if (!firstName || !lastName || !email || !password || 
+        typeof firstName !== 'string' || typeof lastName !== 'string' || typeof email !== 'string' || typeof password !== 'string') {
+        return h.response('Invalid parameter').code(400);
+    }
+
+    // check if email already exists in the database
+    if (await checkEmailExists(email)) {
+        return h.response({error: 'Email already registered. Please use a different email'}).code(200);
+    }
+
+    // Insert user data into the database
+    const insertQuery = 'INSERT INTO `user`(`FirstName`, `LastName`, `Email`, `Password`) VALUES (?, ?, ?, ?)';
+    await connection.execute(insertQuery, [firstName, lastName, email, password]);
+
+        connection.release();
+
+        return h.response({ message: 'Register success' }).code(200);
+
+    } catch (error) {
+        console.error('Error inserting data:', error);
+        return h.response({ error: 'Internal Server Error' }).code(500);
+    }
+};
+
+module.exports = {
+ userRegister
+};
--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const anggota_partai_handler = require('./handlers/anggota_partai_handler.js')
 const partai_handler = require('./handlers/partai_handler.js')
+const user_handler = require('./handlers/user_handler.js')
 
 const init = async () => {
   const server = Hapi.server({
@@ -19,6 +20,11 @@ const init = async () => {
     { method: 'GET', path: '/partai', handler:  partai_handler.getAllPartai},
     { method: 'GET', path: '/partai/{id}', handler: partai_handler.getPartaiById },
   ]);
+
+  server.route([
+    { method: 'POST', path: '/register', handler: user_handler.userRegister },
+    
+  ])
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
